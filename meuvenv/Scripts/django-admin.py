bdc9d00638d270c24c2c0739fef3cefaf8f283b6
adriano silva <adrianlimeira@hotmail.com>
2020-07-30 14:31:25 -0300

    My Django Men app, first commit

---
+++ meuvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\adriano.adriano-pc\desktop\djangomen\meuvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
